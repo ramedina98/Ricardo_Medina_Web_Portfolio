6f3fa15b30c863c0e96a5c9b50a5e3f2563a6198
ramedina98 <b_stone99@outlook.es>
2023-12-16 11:44:15 -0600

    edit

--- README.md
+++ README.md
@@ -5,5 +5,3 @@
 Here is the code of my web porfolio, which serves as my letter of introdyction as a web programer.
 
 :information_source: :information_source:
-
-##
---
+++ js/logic.js
@@ -0,0 +1,73 @@
+/*this js file contains all the code needed to get information 
+from the json file and display the projects I have done so far...*/
+const portfolioSection = document.querySelector('.cont_car_proyects');
+
+//First we get the data from the json...
+const projects = async () => {
+    const url = 'https://ramedina98.github.io/api_nat/projects.json'; 
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        return data; 
+    } catch (error) {
+        console.error('Error: ', error);
+        return []; 
+    }
+
+}
+const stack = (stackItems) => {
+    const elements = stackItems.map(item => `<div class="leng"><i class="${item}"></i></div>`);
+    return elements.join(''); // Concatena todos los elementos del array en una sola cadena sin comas
+};
+const displayTheData = async () => {
+    try {
+        const data = await projects();
+
+        //we build the carts that will contain everything...
+        data.forEach(item => {
+            const projectCart = `
+                <div class="cont_car">
+                <div class="img_cont">
+                    <img src="${item.img}">
+                </div>
+                <div class="information">
+                    <div class="title">
+                        <h3>${item.name}</h3>
+                    </div>
+                    <div class="extracto">
+                        <p>
+                            ${item.description}
+                        </p>
+                    </div>
+                    <div class="tec">
+                        ${stack(item.stack)}
+                    </div>
+                    <div class="links">
+                        <a target="_blank" href="${item.links.github}">
+                            <div class="circle_link">
+                                <i class="fab fa-github icono-github"></i>
+                            </div>
+                        </a>
+                        <a target="_blank" href="${item.links.page}">
+                            <div class="circle_link">
+                                <i class="fas fa-link"></i>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            `
+
+            portfolioSection.insertAdjacentHTML('beforeend', projectCart);
+        });
+        console.log(data[1].stack[1])
+    } catch (error) {
+        console.error('Error in obtaining data', error);
+    }
+}
+
+displayTheData();
